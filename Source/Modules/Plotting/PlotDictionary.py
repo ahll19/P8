0eabb77230d8e81d81f9b812c9751ab13ed62e11
Anders <ahll19@student.aau.dk>
2022-12-16 06:37:56 +0100

    plotting method basics are done

--- Source/Modules/Plotting/PlotDictionary.py
+++ Source/Modules/Plotting/PlotDictionary.py
@@ -1,20 +1,78 @@
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import numpy as np
 
 
 class PlotDictionary:
-    def __init__(self, plotting_dict: dict) -> None:
+    __plot_1d_series_arguments = {
+        "x" : [np.ndarray],
+        "y" : [np.ndarray],
+        "zorder" : [int],
+        "alpha" : [float, int],
+        "ls" : [str],
+        "lw" : [float, int],
+        "marker" : [str],
+        "marker_size" : [float, int],
+        "c" : [str] 
+    }
+    
+    def __init__(self, plotting_dict: dict, backend: str = "TkAgg") -> None:
+        error_string = "plotting_dict should be nested dictionaries"
         for key, value in plotting_dict.items():
-            assert type(value) == dict, "plotting_dict should be nested dictionaries"
+            assert type(value) == dict, error_string
         
         self.plot_dict = plotting_dict
+        self.backend = backend
+    
+    
+    def plot_config(self, grid: bool = False):
+        pass
     
     
     def plot_1d_series(self):
         self.__check_inputs()
+        
+        mpl.use(self.backend)
+        
+        fig, ax = plt.subplots()
+        
+        for outer_key, inner_dict in self.plot_dict.items():
+            ax.plot(
+                inner_dict.pop("x"),
+                inner_dict.pop("y"),
+                label=outer_key,
+                **inner_dict
+            )
+        
+        fig.legend()
+        plt.tight_layout()
+        plt.show()
     
     
     def __check_inputs(self):
-        for key_outer, value_outer in self.plot_dict.items():
-            assert type(value_outer["x"]) == np.array, "Sub-dictionaries must contain key 'x' with value of type np.array"
-            assert type(value_outer["y"]) == np.array, "Sub-dictionaries must contain key 'y' with value of type np.array"
+        for outer_key, inner_dict in self.plot_dict.items():
+            for inner_key, value in inner_dict.items():
+                # make sure each dict contains x and y values
+                error_string = f"Dictionaries must contain \"x\" and \"y\" " \
+                                + f"keys, but missing one in dictionary " \
+                                + f"\"{outer_key}\""
+                x_in = "x" in inner_dict.keys()
+                y_in = "y" in inner_dict.keys()
+                assert x_in and y_in, error_string
+                
+                # Check that all keys are valid
+                error_string = f"Key {inner_key} found in dictionary "\
+                                + f"{outer_key} but method only accepts"\
+                                +f"{self.__plot_1d_series_arguments.keys()}"
+                correct_key = inner_key in self.__plot_1d_series_arguments
+                assert correct_key, error_string
+            
+                # check that all types are valid
+                valid_value_types = self.__plot_1d_series_arguments[inner_key]
+                value_type = type(value)
+                error_string = f"Dictionary {outer_key} has key " \
+                                + f"\"{inner_key}\" with invalid type " \
+                                + f"{value_type}. Only accepts types " \
+                                + f"{valid_value_types}"
+                correct_type = value_type in valid_value_types
+                assert correct_type, error_string
--- Source/test.py
+++ Source/test.py
@@ -5,17 +5,20 @@ import numpy as np
 x = np.linspace(0, 10, 1000)
 y = np.sin(x)
 
-x_ = np.linspace(0, 10, 1000) + np.random.random(1000) / 4
-y_ = np.sin(x_)
+x_ = np.linspace(0, 10, 1000)
+y_ = np.sin(x_) + np.random.uniform(-0.2, 0.2, 1000)
 
 plot_dict = {
     "data clean" : {
         "x" : x,
-        "y" : y
+        "y" : y,
+        "zorder" : 1
     },
     "data noisy" : {
         "x" : x_,
-        "y" : y_
+        "y" : y_,
+        "ls" : "--",
+        "zorder" : 1
     }
 }
 
